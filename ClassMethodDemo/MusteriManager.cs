5cbeb90e219bfe51ff0266cead7cb6472d0b1005
Mustafa ÖZTÜRK <mstfozturk@icloud.com>
2021-02-02 21:21:57 +0300

    Bootcamp Homework

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Id +" numaralı " + musteri.Ad+ " "+musteri.Soyad + " hesabı eklendi");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " numaralı " + musteri.Ad + " " + musteri.Soyad + " hesabı silindi");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("\n"+ musteri.Id + " numaralı " + musteri.Ad + " " + musteri.Soyad + " hesabı");
+            }
+            
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = "123";
+            musteri1.Ad = "Mustafa";
+            musteri1.Soyad = "ÖZTÜRK";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = "234";
+            musteri2.Ad = "Merve";
+            musteri2.Soyad = "Ceylan";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = "345";
+            musteri3.Ad = "Sümbül";
+            musteri3.Soyad = "ÖZTÜRK";
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2,musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteriler);
+                
+
+
+        }
+    }
+}
