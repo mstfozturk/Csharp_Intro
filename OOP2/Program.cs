d749f7301c66c05bc5cf773c04e6ffc25e88a8a8
Mustafa ÖZTÜRK <mstfozturk@icloud.com>
2021-02-04 01:30:37 +0300

    Inheritance - Interface

---
+++ OOP2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mustafa öztürk
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Mustafa";
+            musteri1.Soyadi = "ÖZTÜRK";
+            musteri1.TcNo = "1111111111";
+
+            // Honey Code Coorp.
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Honey Code";
+            musteri2.VergiNo = "1234567890";
+
+
+
+            // Gerçek Müşteri - Tüzel Müşteri
+            // SOLID ==> L
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+        }
+    }
+}
