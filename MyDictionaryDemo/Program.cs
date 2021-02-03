514b974791de24922b4be379b6a6655657f87023
Mustafa ÖZTÜRK <mstfozturk@icloud.com>
2021-02-03 20:27:51 +0300

    MyDictionary Homework

---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> cars = new MyDictionary<string, string>();
+
+            cars.Add("Opel", "Corsa");
+            Console.WriteLine(cars.Length);
+            cars.Add("Volkswagen", "Golf");
+            Console.WriteLine(cars.Length);
+            cars.Add("Tesla", "Model S");
+            Console.WriteLine(cars.Length);
+            cars.Add("Mercedes", "C180");
+            Console.WriteLine(cars.Length);
+            
+            
+
+        }
+    }
+}
